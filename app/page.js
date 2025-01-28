0a6faa52217c56e46ea4de4e72354ca8fa822be5
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-01-28 11:34:59 +0530

    chnges added

--- app/page.js
+++ app/page.js
@@ -1,19 +1,39 @@
+'use client';
+
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import Footer from "@/components/Footer";
 import { About } from "@/components/About";
 import { Scoreboard } from "@/components/Scoreboard";
 import LoadingPage from "@/components/LoadingPage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay for loading
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Adjust the loading duration as needed
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
+
   return (
-    <div className="w-[100%] h-[100svh]">
-      <LoadingPage />
-      <Navbar />
-      <Hero />
-      <About />
-      <Scoreboard />
-      <Footer />
+    <div className="w-full h-[100svh]">
+        {isLoading ? (
+          <LoadingPage /> // Show loader during loading
+        ) : (
+          <>
+            <Navbar />
+            <Hero/>
+            <About/>
+            <Scoreboard/>
+            <Footer />
+          </>
+        )}
     </div>
   );
 }
--- components/LoadingPage.jsx
+++ components/LoadingPage.jsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const LoadingPage = () => {
   const [progress, setProgress] = useState(0);
@@ -14,14 +14,14 @@ const LoadingPage = () => {
         }
         return prev + 1;
       });
-    }, 30); // Adjust speed here (e.g., 30ms for smooth progress)
+    }, 25); // Adjust speed here (e.g., 30ms for smooth progress)
 
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-      <p className="mt-4 text-xl font-semibold text-gray-700">
+      <p className="mt-4 text-8xl font-makalo">
         {progress}%
       </p>
     </div>
