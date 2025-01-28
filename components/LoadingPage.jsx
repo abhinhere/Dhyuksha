7c96a58470f09e9ba285f8ea6cc8e5b636f4e5d2
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-01-28 12:04:38 +0530

    added

--- components/LoadingPage.jsx
+++ components/LoadingPage.jsx
@@ -14,7 +14,7 @@ const LoadingPage = () => {
         }
         return prev + 1;
       });
-    }, 25); // Adjust speed here (e.g., 30ms for smooth progress)
+    }, 20); // Adjust speed here (e.g., 30ms for smooth progress)
 
     return () => clearInterval(interval);
   }, []);
