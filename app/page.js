7cc2f59203f1cc4b695c3fafe34362a12fa8c626
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-01-28 10:48:47 +0530

    added

--- app/page.js
+++ app/page.js
@@ -3,10 +3,12 @@ import { Hero } from "@/components/Hero";
 import Footer from "@/components/Footer";
 import { About } from "@/components/About";
 import { Scoreboard } from "@/components/Scoreboard";
+import LoadingPage from "@/components/LoadingPage";
 
 export default function Home() {
   return (
     <div className="w-[100%] h-[100svh]">
+      <LoadingPage />
       <Navbar />
       <Hero />
       <About />
