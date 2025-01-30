d88f1cdc39c0aed2556d45791ae4b5a10f84a22c
Abhin <abhinchelakkal@gmail.com>
2025-01-30 19:11:08 +0530

    new update:

--- components/leaderboard/leaderboardData.js
+++ components/leaderboard/leaderboardData.js
@@ -1,12 +1,12 @@
 const leaderboardData = [
     { rank: 1, department: 'MCA', points: "18" },
-    { rank: 2, department: 'EC', points: "18" },
-    { rank: 3, department: 'STAFF', points: "10" },
-    { rank: 4, department: 'CE', points: "10" },
-    { rank: 5, department: 'IT', points: "10" },
-    { rank: 6, department: 'CSE', points: "3" },
-    { rank: 7, department: 'DIPLOMA', points: "3" },
-    { rank: 8, department: 'EEE', points: "0" }
+    { rank: 1, department: 'EC', points: "18" },
+    { rank: 2, department: 'STAFF', points: "10" },
+    { rank: 2, department: 'CE', points: "10" },
+    { rank: 2, department: 'IT', points: "10" },
+    { rank: 3, department: 'CSE', points: "3" },
+    { rank: 3, department: 'DIPLOMA', points: "3" },
+    { rank: 4, department: 'EEE', points: "0" }
 ];
 
 export default leaderboardData;
