f50f46abe116d5bf8c5e48e0b0ca8be9b2d75c59
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-02-01 12:05:38 +0530

    vjfv f

--- app/results/page.js
+++ app/results/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Results.css'
 const Results = () => {
     return (
-        <div className='h-screen flex w-full gap-4 pt-20 pb-12 font-makalo text-[#fefefe] flex-col bg-[url("leaderbg.png")] bg-opacity-60 bg-black bg-cover'>
+        <div className='h-screen flex w-full gap-4 pt-20 pb-12  font-makalo text-[#fefefe] flex-col bg-[url("leaderbg.png")] bg-opacity-60 bg-black bg-cover'>
           <div className='list'>
           <p className='game-name'>Tug of War</p>
           <div className='cards'>
