2d114237e3f208d064d3ab00cdea945734f9f952
Abhin <abhinchelakkal@gmail.com>
2025-02-13 08:29:37 +0530

    hfdsfjsdfdsDSWEEW

--- components/leaderboard/leaderboardData.js
+++ components/leaderboard/leaderboardData.js
@@ -1,11 +1,11 @@
 const leaderboardData = [
-    { rank: 1, department: 'CIVIL', points: "36" },
-    { rank: 2, department: 'IT', points: "33" },
+    { rank: 1, department: 'CIVIL', points: "39" },
+    { rank: 2, department: 'IT', points: "38" },
     { rank: 3, department: 'STAFF', points: "28" },
-    { rank: 4, department: 'EC', points: "26" },
-    { rank: 5, department: 'EEE', points: "20" },
-    { rank: 6, department: 'MCA', points: "18" },
-    { rank: 7, department: 'CSE', points: "16" },
+    { rank: 4, department: 'CSE', points: "26" },
+    { rank: 5, department: 'EC', points: "26" },
+    { rank: 6, department: 'EEE', points: "20" },
+    { rank: 7, department: 'MCA', points: "18" },
     { rank: 8, department: 'DIPLOMA', points: "3" }
 ];
 
