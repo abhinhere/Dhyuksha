da14b0442937cf7143c5cb924bc2ec36d39c38e5
Abhin.C <98300668+abhinhere@users.noreply.github.com>
2025-02-13 19:24:30 +0530

    Update leaderboardData.js

--- components/leaderboard/leaderboardData.js
+++ components/leaderboard/leaderboardData.js
@@ -1,8 +1,8 @@
 const leaderboardData = [
-    { rank: 1, department: 'CIVIL', points: "39" },
-    { rank: 2, department: 'IT', points: "38" },
-    { rank: 3, department: 'STAFF', points: "28" },
-    { rank: 4, department: 'CSE', points: "26" },
+    { rank: 1, department: 'IT', points: "48" },
+    { rank: 2, department: 'CIVIL', points: "42" },
+    { rank: 3, department: 'CSE', points: "31" },
+    { rank: 4, department: 'STAFF', points: "28" },
     { rank: 5, department: 'EC', points: "26" },
     { rank: 6, department: 'EEE', points: "20" },
     { rank: 7, department: 'MCA', points: "18" },
