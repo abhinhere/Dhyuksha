c4e3ec8511f1ddc3d6c00b05987db999e8ca71e6
Abhin <abhinchelakkal@gmail.com>
2025-02-13 23:22:54 +0530

    hewfdsf sdfsdf

--- components/leaderboard/leaderboardData.js
+++ components/leaderboard/leaderboardData.js
@@ -1,12 +1,12 @@
 const leaderboardData = [
     { rank: 1, department: 'IT', points: "48" },
     { rank: 2, department: 'CIVIL', points: "42" },
-    { rank: 3, department: 'CSE', points: "31" },
-    { rank: 4, department: 'STAFF', points: "28" },
-    { rank: 5, department: 'EC', points: "26" },
+    { rank: 3, department: 'CSE', points: "34" },
+    { rank: 4, department: 'EC', points: "31" },
+    { rank: 5, department: 'STAFF', points: "28" },
+    { rank: 5, department: 'MCA', points: "28" },
     { rank: 6, department: 'EEE', points: "20" },
-    { rank: 7, department: 'MCA', points: "18" },
-    { rank: 8, department: 'DIPLOMA', points: "3" }
+    { rank: 7, department: 'DIPLOMA', points: "3" }
 ];
 
 export default leaderboardData;
